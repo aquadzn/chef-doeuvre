11f64554eec39c4a9135b857a3c635b68de00b9f
aquadzn <williamjcqs8@gmail.com>
2020-10-27 23:08:58 +0100

    nice

--- app/admin.py
+++ app/admin.py
@@ -13,7 +13,7 @@ from flask import (
 from flask_login import login_required, current_user
 
 from . import db
-from .models import File
+from .models import User, File
 
 
 admin = Blueprint("admin", __name__)
@@ -31,7 +31,48 @@ def index():
 @admin.route("/admin/dashboard")
 @login_required
 def dashboard():
+
     if current_user.id != 1:
         return render_template("404.html"), 404
     else:
-        return render_template("admin/dashboard.html")
+        users = User.query.all()
+        files = File.query.all()
+
+        return render_template("admin/dashboard.html", users=users, files=files)
+
+
+@admin.route("/admin/dashboard/delete_user/<int:user_id>", methods=["POST"])
+@login_required
+def delete_user(user_id):
+
+    if current_user.id != 1:
+        return render_template("404.html"), 404
+    else:
+        User.query.filter_by(id=user_id).delete()
+        db.session.commit()
+
+        logging.info(f"admin - utilisateur #{user_id} supprimé.")
+        flash("Utilisateur supprimée.")
+        return redirect(url_for("admin.dashboard"))
+
+
+@admin.route("/admin/dashboard/delete_file/<int:file_id>", methods=["POST"])
+@login_required
+def delete_file(file_id):
+
+    if current_user.id != 1:
+        return render_template("404.html"), 404
+    else:
+        filepath = os.path.join(
+            "app/static/uploads", File.query.filter_by(id=file_id).first().filename
+        )
+        if os.path.exists(filepath):
+            os.remove(filepath)
+            logging.info(f"admin - {filepath} supprimé.")
+
+        File.query.filter_by(id=file_id).delete()
+        db.session.commit()
+
+        logging.info("admin - record SQL #{file_id} supprimé.")
+        flash("Image supprimée.")
+        return redirect(url_for("admin.dashboard"))
